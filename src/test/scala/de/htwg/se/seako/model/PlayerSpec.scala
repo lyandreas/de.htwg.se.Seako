edd7c20c84f11b71760de06525fdd383cbac3e7b
lyandreas <lyandreas@gmx.de>
2019-04-12 11:37:44 +0200

    removed Fieldsize and FieldsizeSpec

--- src/test/scala/de/htwg/se/seako/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/seako/model/PlayerSpec.scala
@@ -3,5 +3,16 @@ package de.htwg.se.seako.model
 import org.scalatest.{WordSpec, Matchers}
 
 class PlayerSpec extends WordSpec with Matchers{
-
+    "A Player" when { "new "should{
+     val player = Player("Bob", 1)
+      "have a name" in {
+        player.name should be("Bob")
+      }
+      "have a nice String reprensentation" in {
+        player.toString() should be ("Bob")
+      }
+      "and should have the number" in {
+        player.getNumber should be (1)
+      }
+    }}
 }

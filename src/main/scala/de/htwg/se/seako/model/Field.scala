31203dccda376537ae5b910179fa80b7199796c0
nailomat <nail.oezmen@gmail.com>
2019-04-12 09:04:08 +0200

    Creating a playing field

--- src/main/scala/de/htwg/se/seako/model/Field.scala
+++ src/main/scala/de/htwg/se/seako/model/Field.scala
@@ -1,5 +1,12 @@
 package de.htwg.se.seako.model
 
-class Field {
+import javafx.scene.transform.MatrixType
+
+case class Field(fieldsize: Matrix[Cell]) {
+  def this(size:Int) = this(new Matrix[Cell](size, Cell(0)))
+  val size:Int = fieldsize.size
+  def cell(row:Int, col:Int):Cell = fieldsize.cell(row, col)
+  def set (row:Int, col:Int, value:Int):Field = copy(fieldsize.replaceCell(row, col, Cell(Value)))
+  def row (row:Int):
 
 }
--- src/test/scala/de/htwg/se/seako/model/FieldSpec.scala
+++ src/test/scala/de/htwg/se/seako/model/FieldSpec.scala
@@ -1,5 +1,16 @@
 package de.htwg.se.seako.model
 
 class FieldSpec {
+  "A Field is the playingfield of SeaKo. A Field" when {
 
+    "to be constructed" should {
+
+      "be created with the length of its edges as size. Relevant are size 2, 4 and 10" in {
+        val smalField = new Field(2)
+        val mediumField = new Field(4)
+        val bigField = new Field(10)
+      }
+
+    }
+  }
 }

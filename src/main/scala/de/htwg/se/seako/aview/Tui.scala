49cab4d8034fcd442b22c743ed20a5c3a0c9e7d8
nailomat <nail.oezmen@gmail.com>
2019-04-26 10:29:45 +0200

    Tui choose the count of players

--- src/main/scala/de/htwg/se/seako/aview/Tui.scala
+++ src/main/scala/de/htwg/se/seako/aview/Tui.scala
@@ -4,7 +4,16 @@ import de.htwg.se.seako.model.{Field,FieldCreator}
 
 class Tui {
 
-  def players
+  def players(): Unit = {
+    var playercount = 0
+    print("How many players [2|3|4]")
+    val input = readLine()
+    input match {
+      case "2" => playercount = 2
+      case "3" => playercount = 3
+      case "4" => playercount = 4
+    }
+  }
 
 
   def Input(input: String, field:Field):Field = {

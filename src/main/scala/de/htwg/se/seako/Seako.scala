9d26238538850b811d85f4751c206a77ec50f79d
nailomat <nail.oezmen@gmail.com>
2019-04-26 12:37:11 +0200

    Choose the amount of players, name them and put them on the playing field.

--- src/main/scala/de/htwg/se/seako/Seako.scala
+++ src/main/scala/de/htwg/se/seako/Seako.scala
@@ -5,18 +5,18 @@ import de.htwg.se.seako.model.{Field, Player,Cell}
 object Seako {
 
   var field = new Field[Cell](2,Cell(0))
-  var tui = new Tui
   var player = new Player("",0)
+  var tui = new Tui
+
   def main(args: Array[String]): Unit = {
     val projectName = "Seako"
-    println(projectName)
 
     var input : String = ""
 
     do {
       input = readLine()
       field = tui.fieldSize(input, field)
-
+      player = tui.players(input, player)
       println("Field : " + field.toString)
     } while (input != "q")
   }
--- src/main/scala/de/htwg/se/seako/aview/Tui.scala
+++ src/main/scala/de/htwg/se/seako/aview/Tui.scala
@@ -1,25 +1,25 @@
 package de.htwg.se.seako.aview
 
-import de.htwg.se.seako.model.{Field,Cell}
+import de.htwg.se.seako.model.{Field,Cell,Player}
 
 class Tui {
 
 
-  def players() : Unit= {
-    var numberOfPlayers = 0
+  def players(input: String, player: Player):Player = {
+    var number = 0
     print("How many players [2|3|4]")
     val input = readLine()
     input match {
-      case "2" => numberOfPlayers = 2
-      case "3" => numberOfPlayers = 3
-      case "4" => numberOfPlayers = 4
+      case "2" => number = 2
+      case "3" => number = 3
+      case "4" => number = 4
     }
   }
 
   println("Choose size of playing field [little|medium|big]")
 
   def fieldSize(input: String, field:Field[Cell]):Field[Cell] = {
-  //  print("creating a Field wit the size [little|medium|big]")
+
     input match {
       case "little" => new Field[Cell](3, Cell(0))
       case "medium" =>  new Field[Cell](6,Cell(0))

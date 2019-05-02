77d7695bfc0ccce934eea6bfb5a80c3b6d883106
nailomat <nail.oezmen@gmail.com>
2019-05-02 20:56:42 +0200

    FieldCreator should place the players on the field

--- src/main/scala/de/htwg/se/seako/model/FieldCreator.scala
+++ src/main/scala/de/htwg/se/seako/model/FieldCreator.scala
@@ -1,9 +1,16 @@
 package de.htwg.se.seako.model
 
+import de.htwg.se.seako.aview.Tui
+import de.htwg.se.seako.model.{Field,Cell}
 
 class FieldCreator {
 
-  def players(input: String, player: Player) = {
+  var minCol = 0
+  var minRow = 0
+  var maxRow = fieldSize
+  var maxCol = fieldSize
+
+  def setPlayer(input: String, player: Player) = {
     val numberOfPlayers = 0
     val playerOne = Player(name = "Player1", number = 1)
     val playerTwo = Player(name = "Player2", number = 2)
@@ -14,20 +21,22 @@ class FieldCreator {
 
     if (numberOfPlayers == 2) {
       allPlayers = playerOne.name + ", " + playerTwo.name
+
     }
 
     if (numberOfPlayers == 3) {
-      allPlayers = allPlayers + " und " + playerThree.name
+      allPlayers = allPlayers + " and " + playerThree.name
     } else {
       allPlayers = allPlayers + ", " + playerThree.name
     }
 
     if (numberOfPlayers == 4) {
-      allPlayers = allPlayers + " und " + playerFour.name
+      allPlayers = allPlayers + " and " + playerFour.name
     }
 
     println(allPlayers)
 
   }
+
 
 }

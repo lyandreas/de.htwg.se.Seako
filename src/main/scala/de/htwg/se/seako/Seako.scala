40a2b8c0481e91a015211d9e419403090295c108
lyandreas <lyandreas@gmx.de>
2019-05-03 10:58:27 +0200

    removed unnessessary class FieldCreator and FieldCreatorSpec

--- src/main/scala/de/htwg/se/seako/Seako.scala
+++ src/main/scala/de/htwg/se/seako/Seako.scala
@@ -5,11 +5,7 @@ import de.htwg.se.seako.model._
 import de.htwg.se.seako.controller.controllerComponent.Controller
 
 object Seako {
-  /*
-  var field = new Field[Cell](2,Cell(0))
-  var player = new Player("",0)
-  var tui = new Tui
-  */
+
   val controller = new Controller(new Field[Cell](6, Cell()))
   val tui = new Tui(controller)
   controller.notifyObservers
@@ -22,17 +18,6 @@ object Seako {
       input = readLine()
       tui.processInputLine(input)
     } while (input != "q")
-    /*
-    val projectName = "Seako"
-
-    var input : String = ""
-
-    do {
-      input = readLine()
-      field = tui.fieldSize("Hello",field(0,0,Cell[]))
-      //player = tui.players(input, player)
-      println("Field : " + field.toString)
-    } while (input != "q")*/
 
   }
 }
--- src/main/scala/de/htwg/se/seako/aview/Tui.scala
+++ src/main/scala/de/htwg/se/seako/aview/Tui.scala
@@ -3,18 +3,40 @@ package de.htwg.se.seako.aview
 import java.io.BufferedReader
 
 import de.htwg.se.seako.controller.controllerComponent.Controller
-import de.htwg.se.seako.model.{Cell, Field, FieldCreator, Player}
+import de.htwg.se.seako.model.{Cell, Field, Player}
 import de.htwg.se.seako.util.Observer
 
 class Tui(controller: Controller) extends Observer {
   controller.add(this)
-  val size = 93
+  var size = 6
 
   def processInputLine(input: String): Unit = {
     input match {
       case "q" =>
       case "n" => controller.createEmptyField(size)
+      case "s-l" =>
+        size = 3
+        controller.createEmptyField(size)
+      case "s-m" =>
+        size = 6
+        controller.createEmptyField(size)
+      case "s-b" =>
+        size = 9
+        controller.createEmptyField(size)
+      case "p2" =>
+        controller.set(0,0,Cell(1,1,"GREEN"))
+        controller.set(size-1, size-1,Cell(1,2,"RED"))
+      case "p3" =>
+        controller.set(0,0,Cell(1,1,"GREEN"))
+        controller.set(size-1,size-1,Cell(1,2,"RED"))
+        controller.set(0,size-1,Cell(1,3,"BLUE"))
+      case "p4" =>
+        controller.set(0,0,Cell(1,1,"GREEN"))
+        controller.set(size-1,size-1,Cell(1,2,"RED"))
+        controller.set(0,size-1,Cell(1,3,"BLUE"))
+        controller.set(size-1,0,Cell(1,4,"YELLOW"))
       case _ =>
+        println("unknown command")
     }
   }
 

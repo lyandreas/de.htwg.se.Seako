5411c137594059ae04bf8f3307b505ed783c785a
lyandreas <lyandreas@gmx.de>
2019-05-09 16:24:02 +0200

    Added Coveralls and Travis for badget in gitHub

--- src/main/scala/de/htwg/se/seako/model/Cell.scala
+++ src/main/scala/de/htwg/se/seako/model/Cell.scala
@@ -1,7 +1,30 @@
 package de.htwg.se.seako.model
 
+import Console.{GREEN,RED,BLUE,YELLOW_B,RESET}
+
 case class Cell(value: Int = 1,
                 playerNo: Int = 0,
                 color: String = "WHITE",
                 isHighlighted: Boolean = false) {
+  override def toString: String = {
+    var output = ""
+    playerNo match {
+      case 1 =>
+        output = "${RESET}${GREEN}1${RESET}"
+        output
+      case 2 =>
+        output = "${RESET}${RED}2${RESET}"
+        output
+      case 3 =>
+        output = "${RESET}${BLUE}3${RESET}"
+        output
+      case 4 =>
+        output = "${RESET}${YELLOW_B}4${RESET}"
+        output
+      case _ =>
+        output = "-"
+        output
+    }
+
+  }
 }

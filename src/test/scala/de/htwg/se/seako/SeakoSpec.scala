9355c921e561b3bdaecd364fb1e94126be97709f
lyandreas <lyandreas@gmx.de>
2019-05-10 11:34:50 +0200

    added test to SeakoSpec

--- src/test/scala/de/htwg/se/seako/SeakoSpec.scala
+++ src/test/scala/de/htwg/se/seako/SeakoSpec.scala
@@ -7,5 +7,8 @@ class SeakoSpec extends WordSpec with Matchers {
     "accept text input as argument without readline loop, to test it from commandLine " in {
       Seako.main(Array[String]("s"))
     }
+    "quit " in {
+      Seako.main(Array[String]("q")) should be ()
+    }
   }
 }

507a83f31e492d57b0e9723cf20399ec3cf93c32
lyandreas <lyandreas@gmx.de>
2019-05-10 11:27:18 +0200

    added SeakoSpec

---
+++ src/test/scala/de/htwg/se/seako/SeakoSpec.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.seako
+
+import org.scalatest.{WordSpec, Matchers}
+
+class SeakoSpec extends WordSpec with Matchers {
+  "The Seako main class" should {
+    "accept text input as argument without readline loop, to test it from commandLine " in {
+      Seako.main(Array[String]("s"))
+    }
+  }
+}
